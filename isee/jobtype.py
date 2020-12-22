858567ca11fb35e802c859b3497fbf57197bfc47
Tucker Burgin <tburgin@umich.edu>
2020-12-22 12:25:56 -0500

    Further fixes for working directory mutation bug

--- isee/jobtype.py
+++ isee/jobtype.py
@@ -342,7 +342,10 @@ class isEE(JobType):
         # todo: implement possibility of mutating using something other than initial coordinates/topology as a base?
         if '/' in settings.init_topology:   # todo: this shouldn't be necessary because it should already be done in main.init_threads, but apparently it's still necessary?
             settings.init_topology = settings.init_topology[settings.init_topology.rindex('/') + 1:]
-        new_inpcrd, new_top = utilities.mutate(settings.initial_coordinates[0], settings.init_topology, next_step, settings.initial_coordinates[0] + '_' + '_'.join(next_step), settings)
+        initial_coordinates_to_mutate = settings.initial_coordinates[0]
+        if '/' in initial_coordinates_to_mutate:
+            initial_coordinates_to_mutate = initial_coordinates_to_mutate[initial_coordinates_to_mutate.rindex('/') + 1:]
+        new_inpcrd, new_top = utilities.mutate(initial_coordinates_to_mutate, settings.init_topology, next_step, settings.initial_coordinates[0] + '_' + '_'.join(next_step), settings)
 
         # Update history and return
         thread.history.inpcrd.append(new_inpcrd)
